1216be903c5f9060ed5abbe2d83aeae5d083c621
Minh Ta Van <vanminh020698@gmail.com>
2023-10-06 09:47:30 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install squid
+systemctl status squid.service
+sudo sed -i 's/http_access deny all/http_access allow all/g' /etc/squid/squid.conf
+sudo ufw allow 3128
+sudo systemctl restart squid.service
+
