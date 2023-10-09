b5f738c3b374e36a054980d2f1c699f5da297347
Minh Ta Van <30307402+zerominh@users.noreply.github.com>
2023-10-09 20:13:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install squid
-systemctl status squid.service
+# systemctl status squid.service
 sudo sed -i 's/http_access deny all/http_access allow all/g' /etc/squid/squid.conf
 sudo ufw allow 3128
 sudo systemctl restart squid.service
